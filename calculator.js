061038971f0317c2e570e33a94d21b16f14eb035
MevWebDev <szymongrysiewicz@gmail.com>
2024-08-11 18:40:46 +0200

    feat: Add Calculator class with basic arithmetic operations

---
+++ calculator.js
@@ -0,0 +1,16 @@
+class Calculator {
+  constructor() {}
+  add(a, b) {
+    return a + b;
+  }
+  sub(a, b) {
+    return a - b;
+  }
+  multiply(a, b) {
+    return a * b;
+  }
+  divide(a, b) {
+    return a / b;
+  }
+}
+export const calculator = new Calculator();
